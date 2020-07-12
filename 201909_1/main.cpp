1b8248615c447d56be434baa2207d6d67fc3b5c7
chris <wangshiyujd@163.com>
2020-07-12 16:21:07 +0800

    201909_1 finished

--- 201909_1/main.cpp
+++ 201909_1/main.cpp
@@ -1,6 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+struct apple{
+    int nums;
+    int shuguo_num;
+};
+vector<apple> all;
+int T=0, k=-1, P=1;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    scanf("%d %d", &n, &m);
+    all.resize(n);
+    for(int i=0;i<n;++i) {
+        scanf("%d", &all[i].nums);//读初始苹果数
+        all[i].shuguo_num = 0;
+        for(int j=0;j<m;++j) {
+            int curr = 0;
+            scanf("%d", &curr);
+            all[i].nums += curr;
+            all[i].shuguo_num -= curr;
+        }
+        T += all[i].nums;
+        if(all[i].shuguo_num>k) {
+            k = all[i].shuguo_num;
+            P = i + 1;
+        }
+    }
+    printf("%d %d %d\n", T, P, k);
     return 0;
 }
